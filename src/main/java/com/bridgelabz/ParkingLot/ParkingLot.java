8bf64545ce8542e08bded86e9faa253787038bb8
PrajyotGajane <prajyotgajane@gmail.com>
2020-07-17 23:36:00 +0530

    Added: Test case to check if vehicle is parked

---
+++ src/main/java/com/bridgelabz/ParkingLot/ParkingLot.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.ParkingLot;
+
+public class ParkingLot {
+      public boolean parkedVehicle(Object vehicle) {
+            return false;
+      }
+}
--- src/test/java/com/bridgelabz/ParkingLot/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/ParkingLot/ParkingLotTest.java
@@ -1,4 +1,14 @@
 package com.bridgelabz.ParkingLot;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ParkingLotTest {
+      @Test
+      public void givenVehicle_WhenParked_ShouldReturnTrue() {
+            ParkingLot parkingLot = new ParkingLot();
+            Object vehicle = new Object();
+            boolean isParked = parkingLot.parkedVehicle(vehicle);
+            Assert.assertTrue(isParked);
+      }
 }
