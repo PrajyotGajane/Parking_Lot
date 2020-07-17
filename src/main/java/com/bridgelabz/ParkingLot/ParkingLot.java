1efae9defcb8e0cf70d1e87fdf0bc5fa8cf23f70
PrajyotGajane <prajyotgajane@gmail.com>
2020-07-17 23:40:30 +0530

    Refactored: For indentations and spacings

--- src/main/java/com/bridgelabz/ParkingLot/ParkingLot.java
+++ src/main/java/com/bridgelabz/ParkingLot/ParkingLot.java
@@ -2,6 +2,7 @@ package com.bridgelabz.ParkingLot;
 
 public class ParkingLot {
       private Object vehicle;
+
       public boolean parkedVehicle(Object vehicle) {
             this.vehicle = vehicle;
             return true;
