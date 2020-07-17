e44442229a0bb54a4f50e4123d7db7f1d32f7d85
PrajyotGajane <prajyotgajane@gmail.com>
2020-07-18 01:30:34 +0530

    Added: Test case to throw custom exception when car already present

---
+++ src/main/java/com/bridgelabz/ParkingLot/service/ParkingLot.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.ParkingLot.service;
+
+import com.bridgelabz.ParkingLot.exception.ParkingLotException;
+
+import java.util.HashMap;
+
+public class ParkingLot {
+      HashMap<String, String> isVehicleParked = new HashMap<>();
+
+      public boolean parkedVehicle(String vehicleNumber, String owner) throws ParkingLotException {
+            if (isVehicleParked.containsKey(vehicleNumber))
+                  throw new ParkingLotException("Already Parked", ParkingLotException.ExceptionType.ALREADY_PARKED);
+            isVehicleParked.put(vehicleNumber, owner);
+            return isVehicleParked.containsKey(vehicleNumber);
+      }
+}
--- src/test/java/com/bridgelabz/ParkingLot/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/ParkingLot/ParkingLotTest.java
@@ -1,13 +1,26 @@
 package com.bridgelabz.ParkingLot;
 
+import com.bridgelabz.ParkingLot.exception.ParkingLotException;
+import com.bridgelabz.ParkingLot.service.ParkingLot;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ParkingLotTest {
       @Test
-      public void givenVehicle_WhenParked_ShouldReturnTrue() {
+      public void givenVehicle_WhenParked_ShouldReturnTrue() throws ParkingLotException {
             ParkingLot parkingLot = new ParkingLot();
             boolean isParked = parkingLot.parkedVehicle("GA-08-A-2323", "Prajyot");
             Assert.assertTrue(isParked);
+      }
+
+      @Test
+      public void givvenVehicle_WhenAlreadyParked_ShouldThrowCustomException() {
+            ParkingLot parkingLot = new ParkingLot();
+            try {
+                  parkingLot.parkedVehicle("GA-08-A-2323", "Prajyot");
+                  parkingLot.parkedVehicle("GA-08-A-2323", "Prajyot");
+            } catch (ParkingLotException e) {
+                  Assert.assertEquals(e.type, ParkingLotException.ExceptionType.ALREADY_PARKED);
+            }
       }
 }
