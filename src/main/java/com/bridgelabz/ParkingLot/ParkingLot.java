3345ee285ebed2045d34805b94e87dfb3748e64c
PrajyotGajane <prajyotgajane@gmail.com>
2020-07-18 00:44:45 +0530

    Resolved: Conflict issues

--- src/main/java/com/bridgelabz/ParkingLot/ParkingLot.java
+++ src/main/java/com/bridgelabz/ParkingLot/ParkingLot.java
@@ -5,18 +5,13 @@ import java.util.HashMap;
 public class ParkingLot {
       HashMap<String, String> isVehicleParked = new HashMap<>();
 
-<<<<<<< HEAD
-      public boolean parkedVehicle(String vehicleNumber, String carType) {
-            isVehicleParked.put(vehicleNumber, carType);
+      public boolean parkedVehicle(String vehicleNumber, String owner) {
+            isVehicleParked.put(vehicleNumber, owner);
             return isVehicleParked.containsKey(vehicleNumber);
       }
 
       public boolean unparkVehicle(String vehicleNumber) {
             isVehicleParked.remove(vehicleNumber);
-=======
-      public boolean parkedVehicle(String vehicleNumber, String owner) {
-           isVehicleParked.put(vehicleNumber, owner);
->>>>>>> UC_1_Parking_Cars
             return isVehicleParked.containsKey(vehicleNumber);
       }
 }
--- src/test/java/com/bridgelabz/ParkingLot/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/ParkingLot/ParkingLotTest.java
@@ -14,18 +14,14 @@ public class ParkingLotTest {
 
       @Test
       public void givenVehicle_WhenParked_ShouldReturnTrue() {
-<<<<<<< HEAD
-            boolean isParked = parkingLot.parkedVehicle("GA-08-A-2323", "Light");
-=======
             ParkingLot parkingLot = new ParkingLot();
             boolean isParked = parkingLot.parkedVehicle("GA-08-A-2323", "Prajyot");
->>>>>>> UC_1_Parking_Cars
             Assert.assertTrue(isParked);
       }
 
       @Test
       public void givenVehicle_WhenUnparked_ShouldReturnTrue() {
-            parkingLot.parkedVehicle("GA-08-A-2323", "Light");
+            parkingLot.parkedVehicle("GA-08-A-2323", "Prajyot");
             boolean isUnparked = parkingLot.unparkVehicle("GA-08-A-2323");
             Assert.assertEquals(false, isUnparked);
       }
